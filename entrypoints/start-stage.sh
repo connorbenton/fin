c6a6f4336e6eab4cfd5dc9bafe1b4bb75d2a8715
Connor Benton <connor.benton@gmail.com>
2020-06-06 09:41:55 +0000

    reorg structure

--- entrypoints/start-stage.sh
+++ entrypoints/start-stage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set -e
-/usr/share/nginx/html/env.sh &&
+/usr/share/nginx/html/env_load.sh &&
 sqlite_web -H 0.0.0.0 -x /usr/src/app/data.sqlite -d true -u /db &
 (cd /usr/src/app/backend; node index.js) &
 nginx -g "daemon off;"
